a0058d4a4dbca9b60373cdbeb843204c39ac9083
MindRenixStudio <50889667+MindRenixStudio@users.noreply.github.com>
2020-11-06 07:57:56 +0100

    Upload

---
+++ SaveSaviour/ClassDEF/DataImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaveSaviour.ClassDEF
+{
+    public class DataImage
+    {
+        public string GameName { get; set; }
+        public string GamePath { get; set; }
+        public DateTime saveDate { get; set; }
+    }
+}
